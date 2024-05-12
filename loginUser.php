8188aa007aa61f36beb44a0c3c50cfd955b22d1a
karomi <125471614+joiseuyah@users.noreply.github.com>
2024-05-12 22:34:15 +0800

    uupdate pa

--- loginUser.php
+++ loginUser.php
@@ -3,19 +3,19 @@ include("connection.php");
 
 if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
     $userPassword = $_POST['userPassword'];
-    $userEmail = $_POST['userEmail'];
+    $userUsername = $_POST['userUsername'];
 
 
-    if (!empty($userEmail) && !empty($userPassword) && !is_numeric($userEmail)) {
+    if (!empty($userUsername) && !empty($userPassword) && !is_numeric($userUsername)) {
         try {
-            $stmt = $dbh->prepare("SELECT * FROM user_tb WHERE userEmail = :userEmail
+            $stmt = $dbh->prepare("SELECT * FROM user_tb WHERE userUsername = :userUsername
             INNER JOIN user_tb ON user_tb.userID=postuser.uID
             ");
-            $stmt->bindParam(':userEmail', $userEmail);
+            $stmt->bindParam(':userUsername', $userUsername);
             $stmt->execute();
             $user = $stmt->fetch();
 
-            if ($userEmail === $user['userEmail'] AND $userPassword === $user['userPassword']) {
+            if ($userUsername === $user['userUsername'] and $userPassword === $user['userPassword']) {
                 echo "<script type='text/javascript'> alert('Successful Login')</script>";
             } else {
                 echo "<script type='text/javascript'> alert('Login Failed: Incorrect email or password')</script>";
@@ -25,7 +25,6 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
         }
     }
     header("Location: homepageUser.php");
-
 }
 ?>
 
@@ -33,35 +32,69 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ResPAWn - Login</title>
-    <link rel="stylesheet" href="style.css" >
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>ResPAWn - Login</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="loginstyle.css" </head>
+        <script type="text/javascript">
+            function redirectToLogin() {
+                window.location.href = "index.php";
+            }
+        </script>
+
+    <body>
+        <header>
+            <?php
+            include '..\ResPAWn-Project-main\navbar.php';
+            ?>
 </head>
+
 <body>
 
+    <div class="container-wrapper">
+        <div class="container">
+            <div class="row justify-content-end">
+                <div class="col-md-6 offset-md-6">
+                    <div class="form-container text-end">
 
-    <h1>Welcome to ResPAWn!</h1>
-    <h2>Login</h2>
-    <h3>insert slogan kemerut</h3>
+                        <div class="form-title" style="text-align: left;">Login</div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="mb-3 form-floating">
+                                    <input type="text" id="userUsername" class="form-control" name="userUsername" placeholder="Username" required><br><br>
+                                    <label for="userUsername">Username:</label>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="mb-3 form-floating">
+                                    <input type="password" id="userPassword" class="form-control" name="userPassword" placeholder="Password" required><br><br>
+                                    <label for="userPassword">Password:</label>
+                                </div>
+                            </div>
+                        </div>
 
-    <br>
-    <a href="aboutus.php">About Us</a>
-    <br>
+                        <div>
+                            <button type="submit" name="submit"> Login </button>
+                        </div>
 
-    <form method="POST">
+                        <p style="text-align: left;">Don't have an account? <a href="signupUser.php">Signup Here</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <label for="userEmail">Email:</label>
-        <input type="email" id="userEmail" name="userEmail" required><br><br>   
-    
-        <label for="userPassword">Password:</label>
-        <input type="text" id="userPassword" name="userPassword" required><br><br>
+    </p>
+    </div>
+    </div>
 
-        <button type="submit" name="submit"> Submit </button>
-        <p>Don't have an account? <a href="signup.php">Signup Here</a>
+    </div>
 
-        </p>
-</form>
+
 </body>
 
 </html>
