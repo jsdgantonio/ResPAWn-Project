0cb6bd4c46db5f9ee2365560d279fcec3db38a87
ninikoru <152066514+ninikoru@users.noreply.github.com>
2024-05-13 15:33:44 +0800

    (UPDATED) ResPAWn Database

--- respawn_db.sql
+++ respawn_db.sql
@@ -100,12 +100,12 @@ CREATE TABLE `postorg` (
   `orgImage` varchar(45) DEFAULT NULL,
   `orgComment` varchar(45) DEFAULT NULL,
   `orgStatus` varchar(45) DEFAULT NULL,
+  `org_created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`orgPostID`),
   KEY `adding_fk_org_tb` (`oID`),
-  CONSTRAINT `adding_fk` FOREIGN KEY (`oID`) REFERENCES `org_tb` (`orgID`),
   CONSTRAINT `adding_fk_org` FOREIGN KEY (`oID`) REFERENCES `org_tb` (`orgID`),
   CONSTRAINT `adding_fk_org_tb` FOREIGN KEY (`oID`) REFERENCES `org_tb` (`orgID`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -114,7 +114,7 @@ CREATE TABLE `postorg` (
 
 LOCK TABLES `postorg` WRITE;
 /*!40000 ALTER TABLE `postorg` DISABLE KEYS */;
-INSERT INTO `postorg` VALUES (1,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption'),(4,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption'),(5,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption');
+INSERT INTO `postorg` VALUES (1,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption','2024-05-13 05:57:09'),(4,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption','2024-05-13 05:57:09'),(5,1,'morato','dog open for adoption!','dog.png','i want to adopt','Open For Adoption','2024-05-13 05:57:09'),(13,NULL,'nka','adn,k','ndlka',NULL,'arggg','2024-05-13 05:57:09'),(14,NULL,'nka','adn,k','ndlka',NULL,'arggg','2024-05-13 05:57:09'),(15,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(16,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(17,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(18,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(19,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(20,NULL,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(21,1,'nka','adn,k','ndlka',NULL,'arg','2024-05-13 05:57:09'),(29,1,'tama ba to','tama','tama',NULL,'arg','2024-05-13 06:11:48'),(30,1,'may tama ka','may tama ka','may tama ka',NULL,'GOH','2024-05-13 06:12:08'),(31,1,'akosi','akosi','akosi',NULL,'akosi','2024-05-13 06:13:29');
 /*!40000 ALTER TABLE `postorg` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -133,11 +133,12 @@ CREATE TABLE `postuser` (
   `userImage` varchar(45) DEFAULT NULL,
   `userComment` varchar(45) DEFAULT NULL,
   `userStatus` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`userPostID`),
   KEY `adding_fk_user_tb` (`uID`),
   CONSTRAINT `adding_fk_user` FOREIGN KEY (`uID`) REFERENCES `user_tb` (`userID`),
   CONSTRAINT `adding_fk_user_tb` FOREIGN KEY (`uID`) REFERENCES `user_tb` (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -146,6 +147,7 @@ CREATE TABLE `postuser` (
 
 LOCK TABLES `postuser` WRITE;
 /*!40000 ALTER TABLE `postuser` DISABLE KEYS */;
+INSERT INTO `postuser` VALUES (3,2,'kyusi','askal missing dito','askal.png','','missing','2024-05-13 05:55:48'),(17,3,'pearl drive','saklolo yung pusa ko nawawala','pusako.png','nasa ilalim kanina','missing','2024-05-13 05:55:48'),(23,3,'mfklsfk','msfelks','mfkslk',NULL,'arg','2024-05-13 05:55:48'),(28,3,'the one','dog','dog.png',NULL,'missing','2024-05-13 06:11:06'),(29,3,'test','test','test',NULL,'test','2024-05-13 06:11:23');
 /*!40000 ALTER TABLE `postuser` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,7 +167,7 @@ CREATE TABLE `user_tb` (
   `userEmail` varchar(45) DEFAULT NULL,
   `userContact` int(11) DEFAULT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -174,6 +176,7 @@ CREATE TABLE `user_tb` (
 
 LOCK TABLES `user_tb` WRITE;
 /*!40000 ALTER TABLE `user_tb` DISABLE KEYS */;
+INSERT INTO `user_tb` VALUES (1,'lilmarcy','143','marceline','little','marcy@gmail.com',123),(2,'kathy','1010','kathryn','bernardo','kb@gmail.com',234),(3,'hevabi','00','hev','abi','ha@gmail.com',1103),(4,'cerabhie','anakngbayan','alyzza','castillo','aj@gmail.com',378430),(5,'charhashi','afam','bianca','tinsay','bt@gmail.com',289380),(6,'binini','bhdsj','nini','bini','nini@gmail.com',128193),(7,'jeaisbusy','09309','jea','ant','langgam@gmail.com',19201),(8,'langgam','ant','ant','sad','ant@gmail.com',128932),(9,'antyyy','3872','langg','am','anttt@gmail.com',12819),(10,'ysa','4686','ysabella','agb','ya@gmail.com',381);
 /*!40000 ALTER TABLE `user_tb` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -186,4 +189,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-07 23:34:01
+-- Dump completed on 2024-05-13 15:21:23
