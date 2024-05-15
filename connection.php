6f03d6f89ffc89152bd3eaeafa17a5a047ab19d1
AJinomoto00 <150302500+AJinomoto00@users.noreply.github.com>
2024-05-15 19:39:18 +0800

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$port = 3306;
+$socket = "";
+$user = "root";
+$password = "";
+$dbname = "respawn_db";
+
+try {
+    $dbh = new PDO("mysql:host={$host};port={$port};dbname={$dbname}", $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "SUP CUH CONNECTION SUCCESFUL !";
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+?>
