2393b4c0dbfa453682abe6edeb84aa8cffa87a8b
jsdgantonio <152621617+jsdgantonio@users.noreply.github.com>
2024-05-17 18:57:59 +0800

    Update homepageUser.php

--- homepageUser.php
+++ homepageUser.php
@@ -9,6 +9,8 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResPAWn</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="homepageStyle.css" </head>
     <style>
         .comments-section {
             display: none;
