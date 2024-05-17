3143bd6b5cec010be063c9d5c0ef679ba4f0a439
karomi <125471614+joiseuyah@users.noreply.github.com>
2024-05-18 03:30:24 +0800

    no navbar

--- postuser.php
+++ postuser.php
@@ -1,61 +1,61 @@
-<?php 
-session_start();
+<?php
 include("connection.php");
 
-if (!isset($_SESSION['userID'])) {
-    header("Location: login.php");
-    exit;
-}
-
 if (isset($_POST["submit"])) {
-    $userID = $_SESSION['userID'];
-    
+
     $userLocation = $_POST['userLocation'];
     $userCaption = $_POST['userCaption'];
     $userImage = $_POST['userImage'];
+    $userComment = $_POST['userComment'];
     $userStatus = $_POST['userStatus'];
 
     try {
-        $stmt = $dbh->prepare("INSERT INTO postuser (uID, userLocation, userCaption, userImage, userStatus)
-        VALUES (:uID, :userLocation, :userCaption, :userImage, :userStatus)");
 
-        $stmt->bindParam(':uID', $userID);
+        $sql = "INSERT INTO postuser (userLocation, userCaption, userImage, userComment, userStatus)
+        VALUES (:userLocation, :userCaption, :userImage, :userComment, :userStatus)";
+
+        $stmt = $dbh->prepare($sql);
         $stmt->bindParam(':userLocation', $userLocation);
         $stmt->bindParam(':userCaption', $userCaption);
         $stmt->bindParam(':userImage', $userImage);
+        $stmt->bindParam(':userComment', $userComment);
         $stmt->bindParam(':userStatus', $userStatus);
 
         $stmt->execute();
 
-        header("Location: homepageUser.php");
-        exit();
+        header("Location: post.php");
     } catch (PDOException $e) {
-        echo "<script type='text/javascript'>alert('Database Error: " . $e->getMessage() . "');</script>";
+        echo $sql . "<br>" . $e->getMessage();
     }
+    $dbh = null;
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Post User</title>
+    <link rel="stylesheet" href="postuserstyle.css">
 </head>
+
 <body>
-    <br>
     <a href="index.php">Back To Index</a>
-    <br>
-    
     <form method="post">
         <label for="userLocation">Location:</label>
         <input type="text" id="userLocation" name="userLocation" required><br><br>
 
         <label for="userCaption">Caption:</label>
         <input type="text" id="userCaption" name="userCaption" required><br><br>
-        
+
         <label for="userImage">Image:</label>
-        <input type="text" id="userImage" name="userImage" required><br><br>
+        <input type="file" id="userImage" name="userImage" accept="image/*"><br><br>
+
+        <label for="userComment">Comment:</label>
+        <input type="text" id="userComment" name="userComment"><br><br>
 
         <label for="userStatus">Status:</label>
         <input type="text" id="userStatus" name="userStatus" required><br><br>
@@ -63,4 +63,5 @@ if (isset($_POST["submit"])) {
         <button type="submit" name="submit">Submit</button>
     </form>
 </body>
+
 </html>
