51565a8a946a159b6bff884b0ae90e8fa24f3601
ninikoru <152066514+ninikoru@users.noreply.github.com>
2024-05-18 07:47:34 +0800

    upd8s: hinahide na yung pw upon typing it sa login

--- login.php
+++ login.php
@@ -10,23 +10,25 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['submit'])) {
             $stmt = $dbh->prepare("SELECT userID AS id, null AS type, userEmail AS email FROM user_tb WHERE userEmail = :Email AND userPassword = :Password
             UNION ALL
             SELECT orgID AS id, 'org' AS type, orgEmail AS email FROM org_tb WHERE orgEmail = :Email AND orgPassword = :Password");
-
+            
             $stmt->bindParam(':Email', $Email);
             $stmt->bindParam(':Password', $Password);
             $stmt->execute();
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            
+if ($user) {
+    session_start();
+    if ($user['type'] === 'org') {
+        $_SESSION['orgID'] = $user['id'];
+        header("Location: homepageOrg.php");
+    } else {
+        $_SESSION['userID'] = $user['id'];
+        header("Location: homepageUser.php");
+    }
+    exit();
+}
 
-            if ($user) {
-                session_start();
-                if ($user['type'] === 'org') {
-                    $_SESSION['orgID'] = $user['id'];
-                    header("Location: homepageOrg.php");
-                } else {
-                    $_SESSION['userID'] = $user['id'];
-                    header("Location: homepageUser.php");
-                }
-                exit();
-            } else {
+             else {
                 echo "<script type='text/javascript'>alert('Login Failed: Incorrect email or password');</script>";
             }
         } catch (PDOException $e) {
@@ -46,9 +48,8 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['submit'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResPAWn - Login</title>
-    <link rel="stylesheet" href="loginstyle.css">
+    <link rel="stylesheet" href="loginstyle.css" >
 </head>
-
 <body>
 
 
@@ -62,13 +63,13 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['submit'])) {
     <form method="POST">
 
         <label for="Email">Email:</label>
-        <input type="email" id="Email" name="Email" required><br><br>
-
+        <input type="email" id="Email" name="Email" required><br><br>   
+    
         <label for="Password">Password:</label>
-        <input type="text" id="Password" name="Password" required><br><br>
+        <input type="password" id="Password" name="Password" required><br><br>
 
         <button type="submit" name="submit"> Submit </button>
-        <p>Don't have an account? <a href="signup.php">Signup Here</a>
+        <p>Don't have an account? <a href="index.php">Signup Here</a>
 
         </p>
     </form>
