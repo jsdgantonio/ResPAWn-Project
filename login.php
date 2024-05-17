cccd7ef8f30863e7f76c12e3e418a055d1e69c82
karomi <125471614+joiseuyah@users.noreply.github.com>
2024-05-18 04:00:02 +0800

    Update login.php

--- login.php
+++ login.php
@@ -10,25 +10,23 @@ if ($_SERVER['REQUEST_METHOD'] === "POST" && isset($_POST['submit'])) {
             $stmt = $dbh->prepare("SELECT userID AS id, null AS type, userEmail AS email FROM user_tb WHERE userEmail = :Email AND userPassword = :Password
             UNION ALL
             SELECT orgID AS id, 'org' AS type, orgEmail AS email FROM org_tb WHERE orgEmail = :Email AND orgPassword = :Password");
-            
+
             $stmt->bindParam(':Email', $Email);
             $stmt->bindParam(':Password', $Password);
             $stmt->execute();
             $user = $stmt->fetch(PDO::FETCH_ASSOC);
-            
-if ($user) {
-    session_start();
-    if ($user['type'] === 'org') {
-        $_SESSION['orgID'] = $user['id'];
-        header("Location: homepageOrg.php");
-    } else {
-        $_SESSION['userID'] = $user['id'];
-        header("Location: homepageUser.php");
-    }
-    exit();
-}
 
-             else {
+            if ($user) {
+                session_start();
+                if ($user['type'] === 'org') {
+                    $_SESSION['orgID'] = $user['id'];
+                    header("Location: homepageOrg.php");
+                } else {
+                    $_SESSION['userID'] = $user['id'];
+                    header("Location: homepageUser.php");
+                }
+                exit();
+            } else {
                 echo "<script type='text/javascript'>alert('Login Failed: Incorrect email or password');</script>";
             }
         } catch (PDOException $e) {
@@ -48,14 +46,14 @@ if ($user) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResPAWn - Login</title>
-    <link rel="stylesheet" href="style.css" >
+    <link rel="stylesheet" href="loginstyle.css">
 </head>
+
 <body>
 
 
     <h1>Welcome to ResPAWn!</h1>
     <h2>Login</h2>
-    <h3>insert slogan kemerut</h3>
 
     <br>
     <a href="aboutus.php">About Us</a>
@@ -64,8 +62,8 @@ if ($user) {
     <form method="POST">
 
         <label for="Email">Email:</label>
-        <input type="email" id="Email" name="Email" required><br><br>   
-    
+        <input type="email" id="Email" name="Email" required><br><br>
+
         <label for="Password">Password:</label>
         <input type="text" id="Password" name="Password" required><br><br>
 
