f866d2094e156818a242c390e5d0e6b4f9013080
karomi <125471614+joiseuyah@users.noreply.github.com>
2024-05-18 16:02:36 +0800

    Update loginUser.php

--- loginUser.php
+++ loginUser.php
@@ -45,56 +45,184 @@ if ($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])) {
                 window.location.href = "index.php";
             }
         </script>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
 
-    <body>
-        <header>
-            <?php
-            include '..\ResPAWn-Project-main\navbar.php';
-            ?>
-</head>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+
+            body {
+                font-family: 'Poppins', sans-serif;
+                background-color: #ffaf13;
+                color: #333;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                padding: 20px;
+                text-align: center;
+                background-size: cover;
+                background-repeat: repeat-y;
+                background-image: url('background-paw.png');
+            }
+
+            h1 {
+                font-size: 2.5rem;
+                margin-bottom: 10px;
+            }
+
+            h2 {
+                font-size: 1.5rem;
+                margin-bottom: 20px;
+            }
+
+            a {
+                color: #333;
+                text-decoration: none;
+                font-weight: bold;
+                margin-bottom: 20px;
+                display: inline-block;
+            }
+
+            a:hover {
+                text-decoration: underline;
+            }
+
+            .container-wrapper {
+                width: 100%;
+                max-width: 1200px;
+                margin: auto;
+                padding: 20px;
+            }
+
+            .form-container {
+                background-color: rgba(255, 255, 255, .9);
+                padding: 20px;
+                border-radius: 10px;
+                box-shadow: 0 0 30px rgba(0, 0, 0, .15);
+                width: 100%;
+                max-width: 600px;
+                text-align: left;
+            }
+
+            label {
+                display: block;
+                margin-bottom: 10px;
+                font-weight: bold;
+            }
+
+            input[type="text"],
+            input[type="password"] {
+                width: 100%;
+                padding: 10px;
+                margin-bottom: 20px;
+                border: 1px solid #ddd;
+                border-radius: 5px;
+            }
+
+            button {
+                background-color: #333;
+                color: white;
+                border: none;
+                padding: 10px 20px;
+                cursor: pointer;
+                border-radius: 5px;
+                font-weight: bold;
+                width: 100%;
+                font-family: 'Poppins', sans-serif;
+            }
+
+            button:hover {
+                background-color: #555;
+            }
+
+            p {
+                margin-top: 10px;
+            }
+
+            p a {
+                color: #333;
+                text-decoration: underline;
+            }
+
+            @media (max-width: 600px) {
+                .form-container {
+                    padding: 15px;
+                }
+
+                input[type="text"],
+                input[type="password"] {
+                    padding: 8px;
+                }
+
+                button {
+                    padding: 8px 15px;
+                }
+            }
+
+            .text1 {
+                font-family: 'Baloo Bhai', cursive;
+                color: #ffffff;
+                text-shadow: 2px 4px 3px rgba(0, 0, 0, 0.3);
+            }
+        </style>
+    </head>
+
+
 
 <body>
+    <header>
+        <?php
+        include '..\ResPAWn-Project-main\navbar.php';
+        ?>
+        </head>
 
-    <div class="container-wrapper">
-        <div class="container">
-            <div class="row justify-content-end">
-                <div class="col-md-6 offset-md-6">
-                    <div class="form-container text-end">
+        <body>
 
-                        <div class="form-title" style="text-align: left;">Login</div>
-                        <div class="row">
-                            <div class="col-md-6">
-                                <div class="mb-3 form-floating">
-                                    <input type="text" id="userUsername" class="form-control" name="userUsername" placeholder="Username" required><br><br>
-                                    <label for="userUsername">Username:</label>
+            <div class="container-wrapper">
+                <div class="container">
+                    <div class="row justify-content-end">
+                        <div class="col-md-6 offset-md-6">
+                            <div class="form-container text-end">
+
+                                <div class="form-title" style="text-align: left;">Login</div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="mb-3 form-floating">
+                                            <input type="text" id="userUsername" class="form-control" name="userUsername" placeholder="Username" required><br><br>
+                                            <label for="userUsername">Username:</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="mb-3 form-floating">
+                                            <input type="password" id="userPassword" class="form-control" name="userPassword" placeholder="Password" required><br><br>
+                                            <label for="userPassword">Password:</label>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
-                                <div class="mb-3 form-floating">
-                                    <input type="password" id="userPassword" class="form-control" name="userPassword" placeholder="Password" required><br><br>
-                                    <label for="userPassword">Password:</label>
+
+                                <div>
+                                    <button type="submit" name="submit"> Login </button>
                                 </div>
+
+                                <p style="text-align: left;">Don't have an account? <a href="signupUser.php">Signup Here</a></p>
                             </div>
                         </div>
-
-                        <div>
-                            <button type="submit" name="submit"> Login </button>
-                        </div>
-
-                        <p style="text-align: left;">Don't have an account? <a href="signupUser.php">Signup Here</a></p>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-    </p>
-    </div>
-    </div>
+            </p>
+            </div>
+            </div>
 
-    </div>
+            </div>
 
 
-</body>
+        </body>
 
 </html>
