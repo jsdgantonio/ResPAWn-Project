5c3fb55f8343f74b45b4f501dabf48f11bde9f5b
jsdgantonio <152621617+jsdgantonio@users.noreply.github.com>
2024-05-18 13:28:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ResPAWn - Sign Up</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
